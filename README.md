4b3e235995d794f6a2dcc5fcec9663db02f1bf04
rich <dgsimle@sina.com>
2025-03-03 20:44:02 +0800

    demo 优化

--- README.md
+++ README.md
@@ -58,6 +58,7 @@ DioLogInterceptor.enablePrintLog = false;
 <img src="https://raw.githubusercontent.com/flutterplugin/dio_log/develop/images/log_list.jpg" width="200">      
 <img src="https://raw.githubusercontent.com/flutterplugin/dio_log/develop/images/log_request.jpg" width="200">
 <img src="https://raw.githubusercontent.com/flutterplugin/dio_log/develop/images/log_response.jpg" width="200">
+<img src="https://raw.githubusercontent.com/flutterplugin/dio_log/develop/images/log_filter.jpg" width="200">
 
 ### gif demo 
 ![gif](https://raw.githubusercontent.com/flutterplugin/dio_log/develop/images/dio_log_example.gif)
--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -15,13 +15,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController controller = TextEditingController();
 
-  String url = 'https://api.github.com/users';
+  // 添加 API 列表
+  final Map<String, String> apiEndpoints = {
+    'GitHub 用户列表': 'https://api.github.com/users',
+    'JSONPlaceholder 帖子': 'https://jsonplaceholder.typicode.com/posts',
+    '随机用户数据': 'https://randomuser.me/api/',
+    '天气数据(伦敦)': 'https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&forecast_days=1',
+    '狗狗图片': 'https://dog.ceo/api/breeds/image/random',
+    '猫咪图片': 'https://api.thecatapi.com/v1/images/search',
+    '比特币价格': 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT',
+  };
+
+  String selectedApi = 'GitHub 用户列表'; // 默认选中的 API
 
   @override
   void initState() {
     super.initState();
     showDebugBtn(context, btnColor: Colors.blue);
-    controller.text = url;
+    controller.text = apiEndpoints[selectedApi]!;
   }
 
   @override
@@ -40,10 +51,40 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'enter the requesr you want to send and press the send button',
+              '选择一个 API 接口进行测试',
+              style: TextStyle(fontSize: 16),
             ),
-            TextField(
-              controller: controller,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: DropdownButton<String>(
+                value: selectedApi,
+                isExpanded: true,
+                items: apiEndpoints.keys.map((String key) {
+                  return DropdownMenuItem<String>(
+                    value: key,
+                    child: Text(key),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedApi = newValue;
+                      controller.text = apiEndpoints[newValue]!;
+                    });
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'API URL',
+                ),
+                maxLines: 2,
+              ),
             ),
           ],
         ),
@@ -51,7 +92,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _sendRequest,
         child: Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        tooltip: '发送请求',
+      ),
     );
   }
 
